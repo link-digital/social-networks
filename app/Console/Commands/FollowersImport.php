d9555b5d3b5e85497bc98e06a91a3ee1dcb0bb3e
aacevedoday <you@example.com>
2019-05-26 18:42:50 -0500

    uate

--- app/Console/Commands/FollowersImport.php
+++ app/Console/Commands/FollowersImport.php
@@ -58,6 +58,8 @@ class FollowersImport extends Command
                     }
                 }
                 $fields_to_save['network_id'] = 'Facebook';
+                $fields_to_save['nickname'] = $fields_to_save['name'];
+
                 // dd($fields_to_save);
                 if(!Follower::where('network_follower_id',$fields_to_save['network_follower_id'])->first()){
                     $post = Follower::create($fields_to_save);
---
+++ app/Console/Commands/ReactionsPost.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Reaction;
+use App\Post;
+use App\Follower;
+
+
+class ReactionsPost extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reactions:post';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $reactions = Reaction::whereNull('post_id')->take(1000)->get();
+
+        foreach ( $reactions as $key => $reaction ) {
+            $this->info($reaction->id);
+            $post = Post::where('network_post_id', '=', $reaction->network_post_id )->where('network_id', '=', $reaction->network_id)->first();
+            if($post){
+                $this->info(' Post: '. $post->id );
+                $reaction->post_id = $post->id;
+                $follower = Follower::where('network_follower_id', '=', $reaction->network_follower_id )->where('network_id', '=', $reaction->network_id)->first();
+                if($follower){
+                    $this->info('  Follower: '. $follower->id );
+                    $reaction->follower_id = $follower->id;
+                }
+                $reaction->save();
+                
+            }else{
+                $this->info(' No post mached');
+                continue;
+            }
+        }
+    }
+}
