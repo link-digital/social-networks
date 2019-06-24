48e11f8adf9b9fbb8e4ba4a408d0693e99881f05
aacevedoday <you@example.com>
2019-06-24 18:19:18 -0500

    update

---
+++ app/Console/Commands/ReactionsFollowersRelatedDesc.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Follower;
+use App\Reaction;
+
+class ReactionsFollowersRelatedDesc extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'reactions:followers:desc {network_id} {account} {limit=10000}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $network_id = $this->argument('network_id');
+        $account = $this->argument('account');
+        $limit = $this->argument('limit');
+ 
+        $reactions = Reaction::whereNull('follower_id')
+                                ->where('network_id','=',$network_id)
+                                ->where('account','=',$account)
+                                ->take($limit)
+                                ->orderBy('created_at', 'DESC')
+                                ->get();
+
+        foreach ($reactions as $key => $reaction) {
+            $this->info('['.$key.'] Search: '. $reaction->id);
+            // if($network_id = 'Facebook' && $reaction->account='FCF'){
+
+            //     $follower = Follower::where('network_follower_id','=',$reaction->link)
+            //                         ->where('account','=',$reaction->account)
+            //                         ->first();
+            // }else{
+
+            //     $follower = Follower::where('network_follower_id','=',$reaction->network_follower_id)
+            //                         ->where('account','=',$reaction->account)
+            //                         ->first();
+            // }
+            $follower = Follower::where('network_follower_id','=',$reaction->network_follower_id)
+                                ->where('account','=',$reaction->account)
+                                ->first();
+            
+            if($follower){
+                $this->info(' Matched: '. $follower->id);
+                $reaction->follower_id = $follower->id;
+                $reaction->save();
+            }else{
+                $this->info('No Matched');
+            }
+        }
+    }
+}
