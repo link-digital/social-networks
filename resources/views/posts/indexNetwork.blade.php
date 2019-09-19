73e530072a079d8971634670f2763e111b5ea908
aacevedoday <you@example.com>
2019-09-19 11:27:43 -0500

    update

---
+++ resources/views/posts/indexNetwork.blade.php
@@ -0,0 +1,56 @@
+
+@extends('layouts.app', ['activePage' => Request()->network_id , 'titlePage' => __('Posts')])
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">Facebook Posts
+                    <br>
+                    Total: {{$count}}
+                </div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <table class="table"> 
+                        <thead class="thead-dark">
+                            <th>ID</th>
+                            <th>Network</th>
+                            <th>Link</th>
+                            <th>Message</th>
+                            <th>Shares</th>
+                            <th>Likes</th>
+                            <th>Comments</th>
+                            <th>Shares</th>
+                            <th>Comments</th>
+                            <th>Actions</th>
+                        </thead>
+                        <tbody>
+                        @foreach ( $posts as $key => $post )
+                            <tr>
+                                <td>  {{$post->id}} </td> 
+                                <td>  {{$post->network_id}} </td>
+                                <td>  <a href="{{$post->link}}"> ver </a> </td>
+                                <td>  {{$post->message}} </td>
+                                <td>  {{$post->shares}} </td>
+                                <td>  {{$post->likes}} </td>
+                                <td>  {{$post->comments}} </td>
+                                <td>  {{$post->get_shares}} </td>
+                                <td>  {{$post->get_parse}} </td>
+                                <td> <button type="button" class="btn btn-info">Details</button> </td>
+                            </tr>
+                        @endforeach
+                        </tbody>
+                    </table> 
+                    {{ $posts->links() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection --}}
